21f7ce5fbf9d94888d19f71c36d9330504b940fe
sugimoto yuya <yuya-sugimoto@all-connect.jp>
2020-09-03 12:53:39 +0900

    Mod WebRequest Helper (modules) _20200903
    ver 1.2
    
    テストコードの作成

--- Helper/WebRequest/RequestGet.go
+++ Helper/WebRequest/RequestGet.go
@@ -1,6 +1,8 @@
 package WebRequest
 
 import (
+	"bytes"
+	"fmt"
 	"io"
 	"log"
 	"net/http"
@@ -8,17 +10,25 @@ import (
 
 func Request(url string) io.ReadCloser {
 	// Request the HTML page.
-	res, err := http.Get(url)
+	req, err := http.NewRequest("GET", url, nil)
 	if err != nil {
 		log.Fatal(err)
 	}
-	if res != nil {
-		defer res.Body.Close()
-	}
+
+	client := new(http.Client)
+	res, _ := client.Do(req)
+	defer res.Body.Close()
 
 	if res.StatusCode != 200 {
 		log.Fatalf("Status code error: %d %s", res.StatusCode, res.Status)
 	}
 
 	return res.Body
+}
+
+func TestCode(url string) {
+	var testReader io.Reader = Request(url)
+	buf := new(bytes.Buffer)
+	buf.ReadFrom(testReader)
+	fmt.Print(buf.String())
 }
